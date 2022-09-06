02ece5a404c074dc996e05250fd1ab136b6d1979
Kamau Washington <maufau@gmail.com>
2022-09-05 21:29:01 -0400

    Initial commit

---
+++ test/aws-cdk-namespace-deployment-web.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCdkNamespaceDeploymentWeb from '../lib/aws-cdk-namespace-deployment-web-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-namespace-deployment-web-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdkNamespaceDeploymentWeb.AwsCdkNamespaceDeploymentWebStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
