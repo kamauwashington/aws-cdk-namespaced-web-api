8cb08541e879d598e053ca67140a376cdc298563
Kamau Washington <maufau@gmail.com>
2022-09-08 15:02:52 -0400

    updated gitignore to remove .env

--- src/constants/common.constants.ts
+++ src/constants/common.constants.ts
@@ -4,6 +4,6 @@ export const APPLICATION_NAME: string | undefined = process.env.APPLICATION_NAME
 export const CERTIFICATE_ARN: string | undefined = process.env.CERTIFICATE_ARN;
 /*
 Validation should be added to this file to trap any errors prior to deployment
-it is not needed for this excersise as the goal is to show namespaced deployment.
+it is not needed for this excersise as the goal is to show namespaced deployment.   
 */
 
